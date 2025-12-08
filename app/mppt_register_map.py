27dccb7db8bda74cbe9cb0d0cc1bd693a7bd7bd4
carter Lin <loveflee@yahoo.com.tw>
2025-12-08 11:21:27 +0800

    Update mppt_register_map.py

--- app/mppt_register_map.py
+++ app/mppt_register_map.py
@@ -20,7 +20,7 @@ B1_INFO = [
     { "key": "float_voltage", "name": "浮充電壓設定", "unit": "V", "scale": 100, "offset": 20, "length": 2, "signed": False, "ha": {"type": "sensor", "device_class": "voltage"} },
     { "key": "discharge_limit_voltage", "name": "放電電壓下限", "unit": "V", "scale": 100, "offset": 22, "length": 2, "signed": False, "ha": {"type": "sensor", "device_class": "voltage"} },
     
-    { "key": "hw_max_charge_current", "name": "硬體最大充電電流", "unit": "A", "scale": 100, "offset": 24, "length": 2, "signed": False, "ha": {"type": "sensor", "device_class": "current", "icon": "mdi:microchip"} },
+    { "key": "hw_max_charge_current", "name": "硬體最大充電電流", "unit": "A", "scale": 100, "offset": 24, "length": 2, "signed": False, "ha": {"type": "sensor", "device_class": "current", "icon": "mdi:current-dc"} },
     { "key": "max_charge_current", "name": "設定最大充電電流", "unit": "A", "scale": 100, "offset": 26, "length": 2, "signed": False, "ha": {"type": "sensor", "device_class": "current"} },
     { "key": "run_charge_current_limit", "name": "運行充電電流限制", "unit": "A", "scale": 100, "offset": 28, "length": 2, "signed": False, "ha": {"type": "sensor", "device_class": "current"} },
 
